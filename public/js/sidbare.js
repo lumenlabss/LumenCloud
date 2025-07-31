98e933c52a3b07dd305e5f956cfc3d8e7355159c
Sahran <chationofficiel@gmail.com>
2025-07-31 12:08:11 +0200

    FUCK

--- public/js/sidbare.js
+++ public/js/sidbare.js
@@ -1,25 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.querySelector(
     "[data-drawer-toggle='separator-sidebar']"
   );
   const sidebar = document.getElementById("separator-sidebar");
-  const body = document.body;
 
-  let isSidebarOpen = false; // Track the sidebar state
-
-  toggleButton.addEventListener("click", function () {
-    isSidebarOpen = !isSidebarOpen; // Toggle the state
-    sidebar.classList.toggle("-translate-x-full", !isSidebarOpen);
-  });
-
-  body.addEventListener("click", function (event) {
-    if (
-      isSidebarOpen &&
-      !sidebar.contains(event.target) &&
-      !toggleButton.contains(event.target)
-    ) {
-      isSidebarOpen = false; // Update the state
-      sidebar.classList.add("-translate-x-full");
-    }
+  toggleButton.addEventListener("click", () => {
+    sidebar.classList.toggle("-translate-x-full");
   });
 });
